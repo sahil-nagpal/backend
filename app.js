40a39e8f232a57ff898759d6dd770090cebd1c38
sahil-nagpal <sahilnagpal97@gmail.com>
2022-11-27 23:26:01 +0530

    with server database

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ let app = express()
 let PORT = process.env.PORT || 8000;
 let cors = require("cors")
 const moongoose = require('mongoose');
-moongoose.connect("mongodb://localhost:27017/userTable")
+try{
+    moongoose.connect("mongodb+srv://sahil:O0nlcqVY8zwwOL4y@cluster0.bexhm0v.mongodb.net/userTable")
+}
+catch(err){
+    console.log("error in connecting moongoose application")
+}
 app.use(cors())
 app.use(passport.initialize())
 passport.use('jwt',jwt)
